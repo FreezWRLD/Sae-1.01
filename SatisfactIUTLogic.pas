5da76f097bbfb419681950be6f8a88bb713702af
FreezWRLD <mokunojony.xy@gmail.com>
2025-11-29 14:13:53 +0100

    [Enhance] MenuChangerProduction

--- SatisfactIUTLogic.pas
+++ SatisfactIUTLogic.pas
@@ -300,14 +300,29 @@ end;
     repeat
     afficherMenuChangerProduction(page);
     readln(choix);
-      case choix of 
-      //1:changerProductionConstructeur(LingotCuivre);
-      //2:changerProductionConstructeur(LingotFer);
-      //3:changerProductionConstructeur(CableCuivre);
-      //4:changerProductionConstructeur(PlaqueFer);
-      //5:changerProductionConstructeur(TuyauFer);
-      6:if page=1 then menuChangerProduction(2) else menuChangerProduction(1);
-      0:menuDeJeu();
+      case page of 
+      1:begin
+          case choix of 
+          //1:changerProductionConstructeur(LingotCuivre);
+          //2:changerProductionConstructeur(LingotFer);
+          //3:changerProductionConstructeur(CableCuivre);
+          //4:changerProductionConstructeur(PlaqueFer);
+          //5:changerProductionConstructeur(TuyauFer);
+          6:menuChangerProduction(2);
+          0:menuDeJeu();
+          end;
+        end;
+      2:begin
+          case choix of 
+          //1:changerProductionConstructeur(Beton);
+          //2:changerProductionConstructeur(Acier);
+          //3:changerProductionConstructeur(PlaqueRenforcee);
+          //4:changerProductionConstructeur(PoutreIndustrielle);
+          //5:changerProductionConstructeur(Fondation);
+          6:menuChangerProduction(1);
+          0:menuDeJeu();
+          end;
+        end;
       end;
     until choix in [0..6];
   end;
